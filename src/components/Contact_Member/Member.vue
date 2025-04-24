9544db7c4ddb766451fe6889a4d4154ef74fd5c3
GDkelvin <lhquocbao1703@gmail.com>
2025-04-24 22:20:02 +0700

    Add Contact_Member component (member's info)

---
+++ src/components/Contact_Member/Member.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="custom border-3 mx-4 lg:max-w-3xl ">
+        <div class="customStyle flex text-md md:text-xl lg:text-2xl font-bold justify-center border-b-3 py-2 left-[20%]">
+            <p>Trần Kiện Phong</p>-<p>Software Engineer</p>
+        </div>
+        <div class="flex flex-col items-center bg-[#FDFAFB]">
+            <div class="px-8 py-4 text-sm md:text-md lg:text-xl ">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime iusto, cupiditate commodi accusantium
+                consectetur laboriosam reprehenderit, perspiciatis nemo fugiat odit repellat veniam! Dolores inventore
+                sequi ducimus rem dolorum doloribus error!
+            </div>
+
+            <div class="flex gap-2 text-3xl border-1 px-4 py-2 mb-4 rounded-md shadow-md">
+                <a><i class="fa-brands fa-square-github"></i></a>
+                <a><i class="fa-brands fa-linkedin"></i></a>
+                <a><i class="fa-brands fa-discord"></i></a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.custom{
+    box-shadow: 8px 8px 1px 1px;
+}
+</style>
--- src/views/HomePage/components/Body_Home.vue
+++ src/views/HomePage/components/Body_Home.vue
@@ -2,19 +2,44 @@
   <div class="body-content flex flex-col items-center">
     <h1>Body</h1>
     <!-- Test component to see if it works -->
-    <!-- <ButtonText></ButtonText> -->     
-    <PlayingCard :title="title" :desc1="desc1" :desc2="desc2" :is-position-changed="false"></PlayingCard>
+    <!-- <ButtonText></ButtonText> -->  
+    <!-- v-for if having multiple cards  -->
+    <PlayingCard :title = "title"  :desc1="desc1" :desc2="desc2" :is-position-changed="false"></PlayingCard>
+    <Member></Member>
   </div>
 </template>
 
 <script setup lang="ts">
-  import ButtonText from '../../../components/ButtonText/ButtonText.vue'
+  import ButtonText from '@/components/ButtonText/ButtonText.vue'
   import PlayingCard from '@/components/PlayingCard/PlayingCard.vue';
+  import Member from '@/components/Contact_Member/Member.vue';
 
   const title = 'playing card';
   const desc1 = "Play cards, earn points. Reach the checkpoint to complete a stage."
   const desc2 = 'Bring your deck with 49 Basic Cards with 7 Basic Colors from 1 - 7 to challenge with 5 stages. The played card will be lost, so use it wisely!';
-  
+
+  //testing
+  interface PlayingCard{
+    title: string;
+    desc1: string;
+    desc2: string;
+    isPositionChanged: boolean;
+  }
+
+  const playingCard: PlayingCard[] = [
+    {
+      title: 'a',
+      desc1: 'a1',
+      desc2: 'a2',
+      isPositionChanged: true
+    },
+    {
+      title: 'a',
+      desc1: 'a1',
+      desc2: 'a2',
+      isPositionChanged: true
+    }
+  ]
 </script>
 
 <style scope>
